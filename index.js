0f476ae961678eb4be30cfc56c00e80b11e8e3ec
Lucas de Jesus <lucasdjs2014@hotmail.com>
2022-04-12 20:22:24 -0300

    implementações

--- index.js
+++ index.js
@@ -1,13 +1,32 @@
-
 function adicionar(){
+//pega produto no input produto
+    let produto = String(document.getElementsByName('produto')[0].value);
 
-let produto  = document.getElementsByName('produto')[0];
-let primeiro = String(produto.value);
+//Lista produtos no elemento containerProdutos
+let mostrar = document.getElementById('containerProdutos');
 
-var panel =document.createElement('class', 'form-check')
- panel = document.getElementById('mostrarResultado');
-panel.innerHTML = primeiro;
-}
+//cria div
+let divProduto = document.createElement('div')
+//cria o checkbox
+let novoCheckBox = document.createElement('input');
+novoCheckBox.type = 'checkbox';
+//cria a label para produto
+let nomeProduto = document.createElement('label');
+nomeProduto.innerHTML = produto;
+//quebra linha
+let br = document.createElement('br');
+
+divProduto.appendChild(novoCheckBox);
+divProduto.appendChild(nomeProduto);
+divProduto.appendChild(br);
+
+mostrar.appendChild(divProduto)
+
+//apagar oque ta escrito e insere o foco novamente
+document.getElementsByName('produto')[0].value = '';
+    document.getElementsByName('produto')[0].focus();
+    }
+
 
 
 
